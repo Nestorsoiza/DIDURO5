0d0cfa5c3366d0665fc07486929350728e004ae0
Nestorsoiza <nestor.soiza83@gmail.com>
2020-11-29 14:29:50 +0100

    Creo los dos arrays de 5 posiciones

--- src/disco/EjecutableApp.java
+++ src/disco/EjecutableApp.java
@@ -3,6 +3,9 @@ package disco;
 public class EjecutableApp {
 
 	public static void main(String[] args) {
+		Serie listaSeries[] = new Serie[5];
+		Videojuego listaVideojuegos[] = new Videojuego[5];
+		
 
 	}
 
